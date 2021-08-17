8a9e458f4e8a4c7953af91c795396162f0e86bda
abhishek848 <89061870+abhishek65464@users.noreply.github.com>
2021-08-17 16:55:34 +0530

    Update ServicesComp.js

--- src/components/UI/ServicesComp.js
+++ src/components/UI/ServicesComp.js
@@ -10,8 +10,8 @@ const ServicesComp = (props) => {
         img src = { props.imgicn }
         alt = "loading" / >
         <
-        h4 > {
-            [props.hd, props.det] } < /h4>             <
+        h4 > { props.hd } < /h4> <
+        p > { props.det } < /p> <
         /div>
     )
 }
