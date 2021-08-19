d62188a617040e0507406d7f16d242083ec7249f
abhishek848 <89061870+abhishek65464@users.noreply.github.com>
2021-08-19 19:01:37 +0530

    c

--- src/components/footer/DefPtr.js
+++ src/components/footer/DefPtr.js
@@ -1,29 +1,25 @@
 import classes from './DefPtr.module.css';
 
-const DefPtr = (props) => {
+const DefPtr = (props) =>{
 
-    return <div className = { classes.dfCnt } >
-        <
-        div className = { classes.moto } >
-        <
-        h2 > Entire journey of your product at your fingertips < /h2> <
-        p > hello @traceabl.io < /p> <
-        /div> <
-        div className = { classes.service } >
-        <
-        h4 > Services < /h4> <
-        p > Publish your Journey < /p> <
-        p > Supply chain Management < /p> <
-        p > Badge & certificate < /p> <
-        p > Blockchain Traceability Certfication < /p> <
-        /div> <
-        div className = { classes.connect } >
-        <
-        h4 > Contact Us < /h4> <
-        p > hello @traceabl.io < /p> <
-        p > +91 8495066909 < /p> <
-        /div> <
-        /div>
+    return <div className={classes.dfCnt}>
+        <div className={classes.moto}> 
+            <h2>Entire journey of your product at your fingertips</h2>
+            <p>hello@traceabl.io</p>
+        </div>
+        <div className={classes.service}>
+            <h4>Services</h4>
+            <p>Publish your Journey</p>
+            <p>Supply chain Management</p>
+            <p>Badge & certificate</p>
+            <p>Blockchain Traceability Certfication</p>
+        </div>
+        <div className={classes.connect}>
+        <h4>Contact Us</h4>
+        <p>hello@traceabl.io</p>
+        <p>+91 8495066909</p>
+        </div>
+    </div>
 }
 
 export default DefPtr;
--- src/components/footer/Source.js
+++ src/components/footer/Source.js
@@ -1,24 +1,17 @@
 import classes from './Source.module.css';
 import logoImg from '../../assets/logo.png'
-const Source = (props) => {
-    return <div className = { classes.srCnt } >
+const Source = (props) =>{
+    return <div className={classes.srCnt}>
 
-        <
-        div className = { classes.logo } > < img src = { logoImg }
-    alt = "logo" / > < /div>         <
-        ul className = { classes.ftln } >
-        <
-        li > < a href = "/" > Services < /a></li >
-        <
-        li > < a href = "/" > Industry < /a></li >
-        <
-        li > < a href = "/" > About < /a></li >
-        <
-        li > < a href = "/" > Contact Us < /a></li >
-        <
-        /ul> <
-        p > 2021 traceabl.io < /p> <
-        /div>
+        <div className={classes.logo}><img  src={logoImg} alt="logo"/></div>        
+      <ul className={classes.ftln}>
+          <li><a href="/">Services</a></li>
+          <li><a href="/">Industry</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Contact Us</a></li>
+      </ul>
+      <p>2021 traceabl.io</p>
+    </div>
 }
 
 export default Source;
